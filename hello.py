566dc425906aef89f536b68de3287a6fe24d1c52
isoumyadas <sammy8das@gmail.com>
2026-01-13 18:06:59 +0000

    learned variables

--- hello.py
+++ hello.py
@@ -1,9 +1,8 @@
 # print("Hello, World!")
 # print("Hello, Soumya!")
 
+# importing 
+
 import requests
-
 response = requests.get("https://api.github.com")
-
 print(response.status_code)
-
---
+++ variables.py
@@ -0,0 +1,38 @@
+# naming rules allwoed:
+    # user_name = "2"  
+    # userName = "sam" 
+    # age2 = 45   
+    # _private = "secret"
+
+# naming rules not allowed:
+    # 2age = 44 (cannot start with number)                          
+    # my-name = "Dave"  (no hypens [python thinks it's subtraction])        
+    # my name = "Dave" (No spaces)                                      
+    # class = "python"  (Can't use python keywords)
+
+# This is called snak_case
+user_age = 24
+user_name = "sammy"
+is_logged_in = True
+shopping_cart_total = 49.99
+
+# Avoid camalCase [You can use that, but avoid it]
+# userName = "sam"
+
+
+# Variables can change
+
+score = 0
+
+score = 30
+
+score = 120
+
+print("score:: ", score) # this will print 120
+
+
+# Multi-line comments
+"""
+This is multi line comment
+
+"""
