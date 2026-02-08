c0b02c58bf37edd0d2f1f58b2ece0d36721cf79f
isoumyadas <sammy8das@gmail.com>
2026-02-08 03:38:28 +0000

    feat: sloved 7 problems for conditional program

--- 10_problems_conditonal.py
+++ 10_problems_conditonal.py
@@ -45,3 +45,169 @@ elif student_score <= 100:
     print("A")
 else:
     print("Score can't be more than 100")
+
+print("-" * 20)
+
+# 4 : Determine if a fruit is ripe, overripe or unripe based on its color (eg: Banana: Green - Unripe, Yellow - Ripe, Brown - Overripe) 
+
+fruit_ripeness_by_color = {"Green" : "Unripe", "Yellow" : "Ripe", "Brown" : "Overripe"}
+
+fruit_name = "Apple"
+color = "Yellow"
+
+if fruit_name:
+    ripeness = fruit_ripeness_by_color.get(color, "Unkown State")
+    print(f"{fruit_name} is {ripeness}")
+else:
+    print("Please determine any fruit name")
+
+    # Another way to do
+
+fruit = "Apple"
+fruit_color = "Brown"
+
+if fruit == "Apple":
+    if fruit_color == "Green":
+        print(f"{fruit} is Unripe")
+    elif fruit_color == "Yellow":
+        print(f"{fruit} is Ripe")
+    else:
+        print(f"{fruit} is Overripe")
+else:
+    print("Please determine fruit name")
+
+print("-" * 20)
+
+# 5 : Suggest an activity based on the weather (eg: Sunny - Go for walk, Rainy - Read a book, Snowy - Build an snowman)
+
+activity_based_on_weather = {"Sunny" : "Go for walk", "Rainy": "Read a book", "Snowy" : "Build on snowman"}
+
+weather = "Rainy"
+
+if weather:
+    activity = activity_based_on_weather.get(weather, "NONE")
+    print(f"It's {weather} - {activity}")
+else:
+    print("Please Determine Weather!")
+
+print("-" * 20)
+
+# 6 : Choose a mode of transportation based on the distance (eg: <3 km: Walk, 3-15 km: Bike, >15 km: Car)
+
+distance = 2
+
+if distance < 3:
+    print("Walk")
+    # transport = "Walk"
+elif distance <=15:
+    print("Bike")
+else:
+    print("Car")
+
+# print(f"{transport}") # We can use transport variable inside the if else statement.
+
+print("-" * 20)
+
+# 7 : Customize a coffee order: "Small", "Medium", or "Large" with an option for "Extra shot" of expresso.
+
+coffee_size = "Large"
+is_extra_shot = True
+
+# This looks messy:
+
+if coffee_size == "Small":
+    if is_extra_shot:
+        print(f"Your order is small coffee with extra shot of expresso")
+    else:
+        print("Your order is one small coffee")
+elif coffee_size == "Medium":
+    if is_extra_shot:
+        print(f"Your order is Medium coffee with extra shot of expresso")
+    else:
+        print("Your order is one Medium coffee")
+elif coffee_size == "Large":
+    if is_extra_shot:
+        print(f"Your order is Large coffee with extra shot of expresso")
+    else:
+        print("Your order is one Large coffee")
+else:
+    print(f"Please determine correct Coffee size")
+
+# Optimal way to do:
+
+if is_extra_shot:
+    coffee = f"{coffee_size} coffee with extra shot of expresso"
+else: 
+    coffee = f"{coffee_size} coffee"
+
+print("Coffee:", coffee)
+
+
+print("-" * 20)
+
+# 8 : Check if a password is "Weak", "Medium", or "Strong". Criteria: <6 chars (Weak), 6-10 chars (Medium), >10 chars (Strong)
+
+password = "sdfsfadsfasdf"
+password_len = len(password)
+
+if password_len < 6:
+    print("Password strength is weak")
+elif password_len <= 10:
+    print("Password strength is Medium")
+else:
+    print("Password is strong")
+
+
+print("-" * 20)
+
+# 9 : Determine if a year is a leap year. (Leap years are divisible by 4, but not by 100 unless also divisible by 400).
+
+year = 2026
+
+if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+    print(f"{year} is a leap year")
+else:
+    print(f"{year} is not a leap year")
+
+print("-" * 20)
+
+# 10 : Recommend a type of pet food based on the pet's species and age. (eg: DOG:  <2 years - Puppy food, Cat: >5years - Senior cat food).
+
+pet_type = "Cat"
+pet_age = 6
+
+if pet_type == "Dog":
+    if pet_age < 2:
+        print("Puppy food")
+    else:
+        print("Dog Food")
+elif pet_type == "Cat":
+    if pet_age > 5:
+        print("Senior Cat Food")
+    else:
+        print("Junior Cat Food")
+else:
+    print("Species not found")
+
+    # Another way to do:
+pet_food = {
+    "Dog": {
+        "young": "Puppy food",
+        "adult": "Dog food"
+    },
+    "Cat": {
+        "young": "Junior cat food",
+        "senior": "Senior cat food"
+    }
+}
+
+pet_type = "Dog"
+pet_age = 6
+
+if pet_type in pet_food:
+    if pet_type == "Dog":
+        print(pet_food["Dog"]["young"] if pet_age < 2 else pet_food["Dog"]["adult"])
+    else:
+        print(pet_food["Cat"]["senior"] if pet_age > 5 else pet_food["Cat"]["young"])
+else:
+    print("Species not found")
