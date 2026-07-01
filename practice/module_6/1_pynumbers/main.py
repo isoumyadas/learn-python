6fcab5b87d3539235ec5c4e76a4482ae5b884639
isoumyadas <sammy8das@gmail.com>
2026-07-02 03:49:55 +0530

    feat: 1 & 2 done

---
+++ practice/module_6/1_pynumbers/main.py
@@ -0,0 +1,4 @@
+def calculate_damage(sword, arrow, spear, dagger, fireball):
+    total_damage = sword + arrow + spear + dagger + fireball
+    average_damage = (sword + arrow + spear + dagger + fireball)/5 
+    return total_damage, average_damage
